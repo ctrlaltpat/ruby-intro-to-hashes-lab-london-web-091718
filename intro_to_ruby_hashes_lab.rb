7a2fea8b89589669e9fefd143846293eb0c64c72
patrickp246@gmail.com <patrickp246@gmail.com>
2018-09-13 19:31:39 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,7 +10,7 @@ end
 
 def monopoly
 	monopoly = {}
-  monopoly[:railroads => {}]
+  monopoly[:railroads] => {}
 end
 
 def monopoly_with_second_tier
@@ -18,7 +18,7 @@ def monopoly_with_second_tier
 	monopoly = {
 	  railroads: {
 	    :pieces => 4,
-	    :name => {},
+	    :names => {},
 	    :rent_in_dollars => {}
 	  }
 	}
